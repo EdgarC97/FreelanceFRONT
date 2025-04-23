a93b574893b28618d63abde0de27cf007a65351d
EdgarC97 <ecardona742@gmail.com>
2025-04-23 12:10:59 -0500

    generate components for auth and project feature, and shared components

--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectFormComponent } from './project-form/project-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProjectListComponent,
+    ProjectDetailComponent,
+    ProjectFormComponent
+  ],
   imports: [
     CommonModule,
     ProjectsRoutingModule
